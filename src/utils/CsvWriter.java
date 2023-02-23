ae80640fe44fee137feddae27374af992dcd46a4
Gerlene Tamayo <gerlenetamayo@Gerlenes-MacBook-Air.local>
2023-02-23 17:01:39 +0800

    change path

--- src/utils/CsvWriter.java
+++ src/utils/CsvWriter.java
@@ -26,11 +26,11 @@ public class CsvWriter {
                     break;
                 }
                 if (employee_no.equals(employeeObject.employee_no)) {
-                    filePath = "motorph/csv_payroll/admin/";
+                    filePath = "motorph/csv_payroll/employee/";
                     generate_csv(filePath, employeeObject, employee_no);
                     break;
                 } else if(employee_no.equals("admin")) {
-                    filePath = "motorph/csv_payroll/employee/";
+                    filePath = "motorph/csv_payroll/admin/";
                     generate_csv(filePath, employeeObject, employee_no);
                 }
             }
